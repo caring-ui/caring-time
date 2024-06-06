6edfcf53ecafdd15e41dc9ccbaff596a67ce2697
风度万人迷 <805391114@qq.com>
2024-06-06 16:34:18 +0800

    [build] 0.3.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,21 +2,20 @@
  * @Author: Wanko
  * @Date: 2023-02-15 15:55:07
  * @LastEditors: Wanko
- * @LastEditTime: 2024-06-06 15:29:54
+ * @LastEditTime: 2024-06-06 16:33:22
  * @Description: 
  */
-import json from '@rollup/plugin-json'
-import terser from '@rollup/plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
 
 export default {
-  
   input: 'src/index.js',
   output: [
     {
       file: 'dist/index.js',
+      format:'esm'
     }
   ],
-  plugins: [json(), terser(),resolve(), commonjs() ]
+  plugins: [resolve(), commonjs(), terser()]
 }
